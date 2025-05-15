197bec0c240377988798ee4ee12b168bde80153b
satishMohe81 <imsatish811@gmail.com>
2025-05-15 23:52:20 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const TelegramBot = require('node-telegram-bot-api');
+require('dotenv').config();
+
+const token = process.env.BOT_TOKEN;
+const bot = new TelegramBot(token, { polling: true });
+
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, 'Hello from Railway!');
+});
